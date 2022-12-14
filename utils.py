24f03fb73303b0eb7d3d4fa4318b5bfee97a2046
Eliot-shud <119691885+Eliot-shud@users.noreply.github.com>
2022-12-14 23:13:10 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,18 @@
+#импорт json
 import json
 
-
+#возвращает список всех кандидатов
 def load_candidates_from_json() -> list[dict]:
     with open('candidates.json', 'r', encoding='utf-8')as file:
         return json.load(file)
 
-
+#возвращает одного кандидата по его id
 def get_candidate(candidate_id: int) -> dict:
     for candidate in load_candidates_from_json():
         if candidate['id'] == candidate_id:
             return candidate
 
-
+#возвращает кандидатов по имени
 def get_candidates_by_name(candidate_name: str) -> list[dict]:
     result = []
     for candidate in load_candidates_from_json():
@@ -19,7 +20,7 @@ def get_candidates_by_name(candidate_name: str) -> list[dict]:
             result.append(candidate)
     return result
 
-
+#возвращает кандидатов по навыку
 def get_candidates_by_skill(skill_name: str) -> list[dict]:
     result = []
     for candidate in load_candidates_from_json():
